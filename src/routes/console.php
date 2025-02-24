1377049c6459d0ad6b88d7f0685bb9f74e63aff7
yuta-saruwatari <y.s1406@ezweb.ne.jp>
2025-02-25 01:42:35 +0900

    スケジュール,timeout変更

--- src/routes/console.php
+++ src/routes/console.php
@@ -3,4 +3,4 @@
 declare(strict_types=1);
 
 Schedule::command('app:hour-job-run')->cron('50 * * * *')->withoutOverlapping();
-Schedule::command('app:aggregate-run')->dailyAt('23:50');
+Schedule::command('app:aggregate-run')->dailyAt('23:55');
